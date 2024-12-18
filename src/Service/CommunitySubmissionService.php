34c52288fe5bb0174ec0e7b16489ab2e7dc2a1cc
sboesch <sidney.boesch@apollo-dsc.com>
2024-12-18 20:03:22 +0100

    use MAILER_FROM for sending emails / as fallback email

--- src/Service/CommunitySubmissionService.php
+++ src/Service/CommunitySubmissionService.php
@@ -97,7 +97,7 @@ class CommunitySubmissionService
         }
 
         if (!$contactEmail) {
-            $contactEmail = 'info@zukunftsraumland.at';
+            $contactEmail = $this->mailerFrom;
         }
 
         try {
@@ -183,7 +183,7 @@ class CommunitySubmissionService
         };
 
         $email = (new Email())
-            ->from('noreply@zukunftsraumland.at')
+            ->from($this->mailerFrom)
             ->to($submission->getEmail())
             ->subject($subject)
             ->html($this->getEmailTemplate($verificationUrl, $submission->getType()));
